d3b56a6d4083bb7c1069c4c492c6379a72ced343
Archit Sharma <archits1996@gmail.com>
2018-07-15 12:02:30 +0530

    Main Code Upload

---
+++ combining csv.R
@@ -0,0 +1,27 @@
+library(plyr)
+library(dplyr)
+#path - path to the directory
+#recur- set true to include subfolders
+#head- set true if files contains the coloumn names
+#removefiles-  files which you do not want to include
+path="./"
+recur=F
+head=T
+
+
+test<-list.files(path,pattern="*.csv$",recursive = recur)%>%as.data.frame()
+
+removefiles=c("cities.csv")
+
+for(i in 1:length(removefiles)){
+  test<-subset(test,test!=removefiles[i])
+}
+
+data<-data.frame()
+i=1
+for(i in 1:nrow(test)){
+  d1<-read.csv(paste(path,test[i,1],sep="")%>%as.character(),header = head)
+  data<-rbind.fill(data,d1)
+}
+
+write.csv(data,"combined.csv")
